10797737166fb2fb1ddd4571847bf49a8f2b3f40
Haytes <hayterm96@gmail.com>
2019-03-08 18:53:40 -0500

    added weatherID

--- scripts/cleanWeather.py
+++ scripts/cleanWeather.py
@@ -1,8 +1,11 @@
 import csv, os
 import pandas as pd
 
-def getWeatherData(city):
+def getWeatherData(city, entries):
+
+	#Number of rows taken in from the csv
 	ChunkSize = 1000000
+	
 	SOURCE_DIR = '../data/source/weather'
 	STATION_DIR = '../data/cleaned/stations/'
 	WEATHER_DIR = '../data/cleaned/weather/'+city+'/'
@@ -27,8 +30,19 @@ def getWeatherData(city):
 		for chunk in pd.read_csv(file, chunksize=ChunkSize):
 			print("Chunky AF ... ", z)
 			x = chunk[chunk['X.U.FEFF..Station.Name.'].isin(stations)]
-			if x.size > 0:
+			x_size = x.size
+			indexes = list(range(entries, x_size+entries))
+
+			df = df.reset_index(drop=True)
+		#	df.columns[0] = 'New_ID'
+			df['weatherID'] = df.index + entries
+			df.set_index('weatherID')
+			print(df['weatherID'])
+			if x_size > 0:
 				x.to_csv(WEATHER_DIR+city+'_Chunk'+str(z)+'_weather.csv', encoding='utf-8', index=False)
 				z+=1
-getWeatherData('Toronto')
-getWeatherData('Ottawa')
+			entries += x_size
+
+entries = 0
+getWeatherData('Toronto', entries)
+getWeatherData('Ottawa', entries)
