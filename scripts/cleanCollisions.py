42aaadfd5cb63730ce3562f28687dea479112cf3
Francisco Trindade <francisco.ad.trindade@gmail.com>
2019-03-08 14:12:06 -0500

    fuck ds_store

--- scripts/cleanCollisions.py
+++ scripts/cleanCollisions.py
@@ -9,10 +9,6 @@ import datetime
 DATA_DIR = './data'
 SOURCE_DIR = f"{DATA_DIR}/source/collision"
 CLEAN_DIR = f"{DATA_DIR}/cleaned/accident-dim"
-GARBAGE_DIR = f"{DATA_DIR}/garbage"
-ACCIDENT_FILE = f"{CLEAN_DIR}/accident-dim.txt"
-GARBAGE_FILE_ACC = f"{GARBAGE_DIR}/accident-dim.txt"
-GARBAGE_FILE_COLL = f'{GARBAGE_DIR}/collision.txt'
 
 
 def main() -> None:
@@ -21,29 +17,64 @@ def main() -> None:
         print('Run this file from the root directory of the project.')
         return
 
-    csv_files = sorted(os.listdir(SOURCE_DIR))
+    clean_ottawa_collisions()
+
+
+def clean_ottawa_collisions():
+    print("Cleaning Ottawa Collisions")
+    csv_files_raw = os.listdir(SOURCE_DIR)
+    csv_files = []
+    for file in csv_files_raw:
+        if 'ottawa' in file.lower():
+            csv_files.append(file)
+
+    csv_files = sorted(csv_files)
 
     accident_index = 0
     location_index = 0
     locations = {}
     for csv_file in csv_files:
+        is_2017 = False
         if '2017' in csv_file:
-            print(f"Ignoring {csv_file}")
-            continue
+            is_2017 = True
 
         print(f"Cleaning {csv_file}...")
-        accident_index, location_index, locations = clean_file(csv_file, accident_index, location_index, locations)
+        accident_index, location_index, locations = clean_file(csv_file, accident_index, location_index, locations, is_2017)
 
 
-def clean_file(filename, accident_index, location_index, locations):
-    field_names = [
-        "ACCIDENT_ID",
+def clean_file(filename, accident_index, location_index, locations, is_2017):
+    core_field_names = [
         "LOCATION",
         "LONGITUDE",
         "LATITUDE",
         "DATE",
         "TIME",
-        "LOCATION_ID"
+        "ENVIRONMENT",
+        "SURFACE_CONDITION",
+        "TRAFFIC_CONTROL",
+        "LIGHT",
+        "IMPACT_TYPE",
+        "COLLISION_CLASSIFICATION"
+    ]
+
+    field_names = [
+        "ACCIDENT_KEY",
+        "ACCIDENT_TIME",
+        "ENVIRONMENT",
+        "ROAD_SURFACE",
+        "TRAFFIC_CONTROL",
+        "VISIBILITY",
+        "IMPACT_TYPE",
+        "LOCATION_KEY",
+        "LOCATION",
+        "LONGITUDE",
+        "LATITUDE",
+        "DATE",
+        "IS_FATAL",
+        "IS_INTERSECTION",
+        "ROAD_HIGHWAY",
+        "INTERSECTION_RAMP_1",
+        "INTERSECTION_RAMP_2"
     ]
 
     reader = csv.DictReader(open(f"{SOURCE_DIR}/{filename}"))
@@ -52,31 +83,77 @@ def clean_file(filename, accident_index, location_index, locations):
 
     for row in reader:
         # Accident ID
-        new_row = get_elements(row)
-        new_row["ACCIDENT_ID"] = accident_index
+        new_row = get_elements(row, core_field_names)
+        new_row["ACCIDENT_KEY"] = accident_index
         accident_index += 1
+
+        # Rename Keys   
+        new_row["ACCIDENT_TIME"] = new_row.pop("TIME")
+        new_row["ROAD_SURFACE"] = new_row.pop("SURFACE_CONDITION")
+        new_row["VISIBILITY"] = new_row.pop("LIGHT")
 
         # Location ID
         new_location = f"{new_row['LONGITUDE']}-{new_row['LATITUDE']}"
         if new_location not in locations:
             locations[new_location] = location_index
-            new_row["LOCATION_ID"] = location_index
+            new_row["LOCATION_KEY"] = location_index
             location_index += 1
         else:
-            new_row["LOCATION_ID"] = locations[new_location]
+            new_row["LOCATION_KEY"] = locations[new_location]
 
         # Clean Time
-        new_row["TIME"], next_day = parse_time(new_row["TIME"])
+        new_row["ACCIDENT_TIME"], next_day = parse_time(new_row["ACCIDENT_TIME"])
 
-        if next_day:
-            new_date = datetime.datetime.strptime(new_row["DATE"], "%Y-%m-%d")
-            new_date = new_date + datetime.timedelta(days=1)
-            new_row["DATE"] = new_date.strftime("%Y-%m-%d")
+        new_row["DATE"] = normalize_date(new_row["DATE"], next_day, is_2017)
+
+        # Determine fatality
+        if " fatal injury" in new_row["COLLISION_CLASSIFICATION"].lower():
+            new_row["IS_FATAL"] = True
+        else:
+            new_row["IS_FATAL"] = False
+
+        # Clean up unnecesary ids from source
+        for key in ["ENVIRONMENT", "ROAD_SURFACE", "TRAFFIC_CONTROL", "VISIBILITY", "IMPACT_TYPE"]:
+            new_row[key] = parse_out_id(new_row[key])
+
+        # Clean up location
+        # parse_location(new_row["LOCATION"])
+
+        # Remove some keys
+        new_row.pop("COLLISION_CLASSIFICATION")
 
         # Write row to file
         writer.writerow(new_row)
 
     return accident_index, location_index, locations
+
+def parse_location(location):
+    is_intersection = False
+    road_highway_name = ''
+    intersection_ramp_1 = ''
+    intersection_ramp_2 = ''
+
+    if '@' in location:
+        is_intersection = True
+        location = location.split('@')
+        if '/' in location[0] and '/' in location[1]:
+            print('@'.join(location))
+
+    
+def normalize_date(date, next_day, is_2017):
+    if next_day == False and is_2017 == False:
+        return date
+
+    in_format = "%Y-%m-%d"
+    if is_2017:
+        in_format = "%m/%d/%Y"
+    
+    new_date = datetime.datetime.strptime(date, in_format)
+    
+    if next_day:
+        new_date = new_date + datetime.timedelta(days = 1)
+    
+    return new_date.strftime("%Y-%m-%d")
 
 def parse_time(time_in):
     if len(time_in.split(':')[0]) == 1:
@@ -106,18 +183,15 @@ def convert24(time_in):
     else:
         return str(int(time_in[:2]) + 12) + time_in[2:8]
 
+def parse_out_id(val_in):
+    if val_in == '':
+        return ''
+    return val_in.split(' - ')[1]
 
-def get_elements(row):
-    elements = [
-        "LOCATION",
-        "LONGITUDE",
-        "LATITUDE",
-        "DATE",
-        "TIME"
-    ]
 
+def get_elements(row, field_names):
     new_row = {}
-    for key in elements:
+    for key in field_names:
         new_row[key] = row[key]
 
     return new_row
